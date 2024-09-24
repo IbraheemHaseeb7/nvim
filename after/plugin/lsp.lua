bba13c5216fd4a369bca4f229c1ef951001679bb
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-09-23 23:52:15 -0700

    initial push

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,30 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "intelephense", "lua_ls", "gopls", "ts_ls", "clangd" }
+})
+
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").intelephense.setup {}
+require("lspconfig").gopls.setup {}
+require("lspconfig").ts_ls.setup {}
+require("lspconfig").clangd.setup {}
+
+local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+
+--lsp_zero.preset('recommended')
+
+cmp.setup({
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item(),  -- Move to next item
+        ['<C-p>'] = cmp.mapping.select_prev_item(),  -- Move to previous item
+        ['<C-Space>'] = cmp.mapping.complete(),        -- Trigger completion
+        ['<C-e>'] = cmp.mapping.close(),               -- Close completion
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+    },
+    sources = {
+        { name = 'nvim_lsp' },  -- LSP source
+        { name = 'buffer' },     -- Buffer source
+        { name = 'path' },       -- Path source
+    },
+})
